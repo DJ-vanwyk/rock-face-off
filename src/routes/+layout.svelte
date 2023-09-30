3214fc48657c636012018a18ac33309ef841906e
DJ-V2Tech <djvanwyk100@gmail.com>
2023-09-30 19:39:08 +0200

    Home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,74 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, TabGroup, TabAnchor, Avatar } from '@skeletonlabs/skeleton';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	import Auth from '$lib/components/auth/Auth.svelte';
+	import { page } from '$app/stores';
+	import Icon from '@iconify/svelte';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
 <!-- App Shell -->
 <Auth>
-	<AppShell>
+	<AppShell regionPage="no-scrollbar">
 		<!-- Page Route Content -->
 		<slot />
+		<svelte:fragment slot="footer">
+			<TabGroup
+				justify="justify-center"
+				active="text-primary-500"
+				hover="hover:variant-soft-primary"
+				flex="flex-1 lg:flex-none"
+				border=""
+				rounded=""
+				class="bg-surface-50 w-full"
+			>
+				<TabAnchor href="/" selected={$page.url.pathname === '/'}>
+					<svelte:fragment slot="lead">
+						<div class="flex justify-center pt-1">
+							<Icon icon="ion:home" class="text-2xl text-center" />
+						</div>
+					</svelte:fragment>
+					<span class="text-sm">Home</span>
+				</TabAnchor>
+				<TabAnchor href="/" selected={$page.url.pathname === '/profile'}>
+					<svelte:fragment slot="lead">
+						<div class="flex justify-center pt-1">
+							<Icon icon="ion:trophy" class="text-2xl text-center" />
+						</div>
+					</svelte:fragment>
+					<span class="text-sm">Comps</span>
+				</TabAnchor>
+				<TabAnchor href="/" selected={$page.url.pathname === '/profile'}>
+					<svelte:fragment slot="lead">
+						<div class="flex justify-center pt-1">
+							<Icon icon="ion:person" class="text-2xl text-center" />
+						</div>
+					</svelte:fragment>
+					<span class="text-sm">Profile</span>
+				</TabAnchor>
+				<TabAnchor href="/" selected={$page.url.pathname === '/profile'}>
+					<!-- <svelte:fragment slot="lead">
+						<div class="flex justify-center pt-1">
+							<Icon icon="ion:person" class="text-2xl text-center" />
+						</div>
+					</svelte:fragment> -->
+					<div class="flex justify-center items-center h-full">
+						<Avatar
+							initials="dj"
+							width="w-12"
+							background="variant-filled-primary"
+							class="text-white"
+						/>
+					</div>
+					<!-- <span class="text-sm">Profile</span> -->
+				</TabAnchor>
+
+				<!-- ... -->
+			</TabGroup>
+		</svelte:fragment>
 	</AppShell>
 </Auth>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import CompCard from '$lib/components/CompCard.svelte';
 	import { db } from '$lib/firebase';
+	import Icon from '@iconify/svelte';
 	import { collection, getDocs, query, where } from 'firebase/firestore';
 	import { onMount } from 'svelte';
 
@@ -11,20 +12,57 @@
 
 		const querySnapshot = await getDocs(q);
 
-		querySnapshot.forEach((doc) => {
-			// doc.data() is never undefined for query doc snapshots
-			let comp = doc.data();
-			comp['id'] = doc.id;
+		// querySnapshot.forEach((doc) => {
+		// 	// doc.data() is never undefined for query doc snapshots
+		// 	let comp = doc.data();
+		// 	comp['id'] = doc.id;
 
-			console.log(comp);
+		// 	console.log(comp);
 
-			competitions = [...competitions, comp];
-		});
+		// 	competitions = [...competitions, comp];
+		// });
 	});
 </script>
 
 <div class="p-4">
+	<img src="/logos/logo-no-background.svg" alt="" class="w-40 py-4" />
+
+	<h1 class="text-3xl pb-4">Welcome back, DJ!</h1>
+
+	<div class="flex justify-between items-center pb-4">
+		<h1 class="text-lg">Your Competitions</h1>
+		<button
+			class="
+            flex
+            gap-1
+            items-center
+            text-primary-500
+        "
+		>
+			All
+			<Icon icon="ion:chevron-forward" />
+		</button>
+	</div>
 	{#each competitions as comp}
 		<CompCard {comp} />
 	{/each}
+
+	<div class="overflow-auto no-scrollbar snap-x">
+		<div class="flex w-fit gap-3">
+			<div class="card h-56 variant-filled-tertiary mb-4 w-52 card-hover snap-start" />
+			<div class="card h-56 variant-filled-tertiary mb-4 w-52 card-hover snap-start" />
+			<div class="card h-56 variant-filled-tertiary mb-4 w-52 card-hover snap-start" />
+			<div class="card h-56 variant-filled-tertiary mb-4 w-52 card-hover snap-start" />
+			<div class="card h-56 variant-filled-tertiary mb-4 w-52 card-hover snap-start" />
+		</div>
+	</div>
+	<h1 class="text-lg pb-4">New @ your gym</h1>
+
+	<div class="card h-56 variant-filled-tertiary mb-4" />
+	<div class="card h-56 variant-filled-tertiary mb-4" />
+	<div class="card h-56 variant-filled-tertiary mb-4" />
+	<div class="card h-56 variant-filled-tertiary mb-4" />
+	<div class="card h-56 variant-filled-tertiary mb-4" />
+	<div class="card h-56 variant-filled-tertiary mb-4" />
+	<div class="card h-56 variant-filled-tertiary mb-4" />
 </div>
