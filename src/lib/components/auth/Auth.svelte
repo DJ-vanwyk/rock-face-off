5c50532f830f36769ed38f82a7e8fc906216f507
DJ-V2Tech <djvanwyk100@gmail.com>
2023-09-30 15:43:20 +0200

    Auth Pages

---
+++ src/lib/components/auth/Auth.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { authPageStore } from '$lib/stores/authPage.store';
+	import Login from './Login.svelte';
+	import Signup from './Signup.svelte';
+</script>
+
+{#if $authPageStore === 'login'}
+	<Login />
+{:else if $authPageStore === 'signup'}
+	<Signup />
+{:else}
+	<slot />
+{/if}
---
+++ src/lib/components/auth/Login.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { authPageStore } from '$lib/stores/authPage.store';
+	import Icon from '@iconify/svelte';
+
+	function onRegister() {
+		$authPageStore = 'signup';
+	}
+</script>
+
+<div class="w-screen h-screen flex p-6 justify-center items-center">
+	<form class="w-full">
+		<h1 class="text-3xl font-bold pb-2">Sign In</h1>
+		<p class="pb-6">
+			Don't have an account?
+			<button class="text-blue-500 font-medium" on:click={onRegister} type="button">Register</button
+			>
+		</p>
+		<button class="btn variant-outline-secondary w-full mb-4"
+			><Icon icon="flat-color-icons:google" class="mr-2 text-2xl" /> Sign In with Google</button
+		>
+		<div class="relative py-4">
+			<hr class="!border-t-2 or" />
+			<span
+				class="absolute text-center left-0 right-0 top-0 bottom-0 flex justify-center items-center"
+			>
+				<span class="px-4 bg-surface-50">OR</span>
+			</span>
+		</div>
+		<label class="label mt-4 mb-2">
+			<span>Email</span>
+			<input class="input" type="text" placeholder="" />
+		</label>
+		<label class="label">
+			<span>Password</span>
+			<input class="input" type="password" placeholder="" />
+		</label>
+		<div class="flex justify-end py-1">
+			<button class="text-blue-500">Forget password?</button>
+		</div>
+		<button class="btn variant-filled-primary w-full mt-7">Login</button>
+	</form>
+</div>
