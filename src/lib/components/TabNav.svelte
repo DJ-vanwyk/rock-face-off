7883e770068555512ef22d224051240212b250b7
DJ-V2Tech <djvanwyk100@gmail.com>
2023-09-30 21:24:18 +0200

    Comp card

---
+++ src/lib/components/TabNav.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { page } from '$app/stores';
+	import Icon from '@iconify/svelte';
+	import { Avatar, TabAnchor, TabGroup } from '@skeletonlabs/skeleton';
+</script>
+
+<TabGroup
+	justify="justify-center"
+	active="text-primary-500"
+	hover="hover:variant-soft-primary"
+	flex="flex-1 lg:flex-none"
+	border=""
+	rounded=""
+	class="bg-surface-50 w-full"
+>
+	<TabAnchor href="/" selected={$page.url.pathname === '/'}>
+		<svelte:fragment slot="lead">
+			<div class="flex justify-center pt-1">
+				<Icon icon="ion:home" class="text-2xl text-center" />
+			</div>
+		</svelte:fragment>
+		<span class="text-xs">Home</span>
+	</TabAnchor>
+	<TabAnchor href="/competitions" selected={$page.url.pathname === '/competitions'}>
+		<svelte:fragment slot="lead">
+			<div class="flex justify-center pt-1">
+				<Icon icon="ion:trophy" class="text-2xl text-center" />
+			</div>
+		</svelte:fragment>
+		<span class="text-xs">Comps</span>
+	</TabAnchor>
+	<TabAnchor href="/competitions" selected={$page.url.pathname === '/gyms'}>
+		<svelte:fragment slot="lead">
+			<div class="flex justify-center pt-1">
+				<Icon icon="map:gym" class="text-2xl text-center" />
+			</div>
+		</svelte:fragment>
+		<span class="text-xs">Gyms</span>
+	</TabAnchor>
+
+	<TabAnchor href="/" selected={$page.url.pathname === '/profile'}>
+		<!-- <svelte:fragment slot="lead">
+						<div class="flex justify-center pt-1">
+							<Icon icon="ion:person" class="text-2xl text-center" />
+						</div>
+					</svelte:fragment> -->
+		<div class="flex justify-center items-center h-full">
+			<Avatar initials="dj" width="w-12" background="variant-filled-primary" class="text-white" />
+		</div>
+	</TabAnchor>
+
+	<!-- ... -->
+</TabGroup>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,7 @@
 	import Auth from '$lib/components/auth/Auth.svelte';
 	import { page } from '$app/stores';
 	import Icon from '@iconify/svelte';
+	import TabNav from '$lib/components/TabNav.svelte';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
@@ -17,50 +18,7 @@
 		<!-- Page Route Content -->
 		<slot />
 		<svelte:fragment slot="footer">
-			<TabGroup
-				justify="justify-center"
-				active="text-primary-500"
-				hover="hover:variant-soft-primary"
-				flex="flex-1 lg:flex-none"
-				border=""
-				rounded=""
-				class="bg-surface-50 w-full"
-			>
-				<TabAnchor href="/" selected={$page.url.pathname === '/'}>
-					<svelte:fragment slot="lead">
-						<div class="flex justify-center pt-1">
-							<Icon icon="ion:home" class="text-2xl text-center" />
-						</div>
-					</svelte:fragment>
-					<span class="text-sm">Home</span>
-				</TabAnchor>
-				<TabAnchor href="/competitions" selected={$page.url.pathname === '/competitions'}>
-					<svelte:fragment slot="lead">
-						<div class="flex justify-center pt-1">
-							<Icon icon="ion:trophy" class="text-2xl text-center" />
-						</div>
-					</svelte:fragment>
-					<span class="text-sm">Comps</span>
-				</TabAnchor>
-
-				<TabAnchor href="/" selected={$page.url.pathname === '/profile'}>
-					<!-- <svelte:fragment slot="lead">
-						<div class="flex justify-center pt-1">
-							<Icon icon="ion:person" class="text-2xl text-center" />
-						</div>
-					</svelte:fragment> -->
-					<div class="flex justify-center items-center h-full">
-						<Avatar
-							initials="dj"
-							width="w-12"
-							background="variant-filled-primary"
-							class="text-white"
-						/>
-					</div>
-				</TabAnchor>
-
-				<!-- ... -->
-			</TabGroup>
+			<TabNav />
 		</svelte:fragment>
 	</AppShell>
 </Auth>
