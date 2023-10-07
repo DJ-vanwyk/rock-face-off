54637ec329249357b5786b3ff7fab7c4d6ab75fa
DJ-V2Tech <djvanwyk100@gmail.com>
2023-10-07 19:54:29 +0200

    Competition registration check

--- src/routes/competitions/[id]/+page.svelte
+++ src/routes/competitions/[id]/+page.svelte
@@ -1,11 +1,36 @@
 <script lang="ts">
+	import { databases } from '$lib/appwrite';
+	import { user } from '$lib/stores/user.store';
 	import Icon from '@iconify/svelte';
+	import { Query } from 'appwrite';
 
 	export let data: any;
 
 	let competition = data.competition;
 
-	console.log(data);
+	let isRegisteredPromise = checkEntry();
+
+	async function checkEntry() {
+		if ($user) {
+			try {
+				const resp = await databases.listDocuments('652128d14c078883668a', '65213e03b1f3ab84700f', [
+					Query.equal('userId', $user?.account.$id),
+					Query.equal('competition', competition.$id)
+				]);
+
+				if (resp.total !== 0) {
+					return true;
+				} else {
+					return false;
+				}
+			} catch (error) {
+				console.log(error);
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
 </script>
 
 <img src={competition.imgUrl} alt="" class="h-60 w-full object-cover" />
@@ -15,7 +40,19 @@
 <div class="p-4">
 	<h1 class="text-2xl font-bold">{competition.name}</h1>
 	<h2 class="mb-3">{competition.organization}</h2>
-	<a href={`${competition.$id}/enter`} class="btn variant-filled-primary w-full mb-6">Enter Now</a>
+	{#await isRegisteredPromise}
+		<div class="placeholder mb-6 h-[42px]" />
+	{:then isRegistered}
+		{#if isRegistered}
+			<a href={`${competition.$id}/enter`} class="btn variant-filled-secondary w-full mb-6">
+				My Scores
+			</a>
+		{:else}
+			<a href={`${competition.$id}/enter`} class="btn variant-filled-primary w-full mb-6">
+				Enter Now
+			</a>
+		{/if}
+	{/await}
 	<p class="mb-6 text-sm">{competition.description}</p>
 	<h3 class="font-bold mb-2">Detail:</h3>
 
--- src/routes/competitions/[id]/+page.ts
+++ src/routes/competitions/[id]/+page.ts
@@ -1,11 +1,8 @@
 import { databases } from '$lib/appwrite';
-import { user } from '$lib/stores/user.store.js';
-import { Query } from 'appwrite';
 
 export async function load({ params }) {
 	let document = undefined;
 	let error = [];
-	let isRegistered = false;
 
 	// Get Competition
 	try {
@@ -19,28 +16,9 @@ export async function load({ params }) {
 	}
 
 	// Get User Entry
-	try {
-		const userId = user.getValue()?.account.$id;
-
-		if (userId) {
-			document = await databases.listDocuments('652128d14c078883668a', '65213e03b1f3ab84700f', [
-				Query.equal('competition', params.id),
-				Query.equal('userId', userId)
-			]);
-
-			console.log(document);
-
-			if (document.total !== 0) {
-				isRegistered = true;
-			}
-		}
-	} catch (err) {
-		error.push(err);
-	}
 
 	return {
 		competition: document,
-		error,
-		isRegistered
+		error
 	};
 }
